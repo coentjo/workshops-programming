d8748854a2b2ef5008c644473bee76da8d5e93fa
Crombach <C.Crombach@Fontys.nl>
2025-02-25 13:19:10 +0100

    CSharp-initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Simple counting program
+Console.WriteLine("Welcome to the counting program!");
+
+// Loop from 1 to 5
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Number: {i}");
+    
+    // Add a small delay to make it easier to see each number
+    Thread.Sleep(500);
+}
+
+Console.WriteLine("Counting finished!");
+Console.WriteLine("Press the any key to exit...");
+Console.ReadKey();
